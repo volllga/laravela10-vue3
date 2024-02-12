fd8d19c816e0a4dcd91753077e0705b6d74b4367
Volha Lukashevich <volllga.goo@gmail.com>
2024-02-12 09:06:01 +0100

    Javascript и Vuejs для Laravel 17. Асинхронный CRUD. Обновление персоны. #1 Переключатель на форму

--- resources/js/components/TableComponent.vue
+++ resources/js/components/TableComponent.vue
@@ -8,6 +8,7 @@
                 <th scope="col">Category Name</th>
                 <th scope="col">Active</th>
                 <th scope="col">Created at</th>
+                <th scope="col">Actions</th>
             </tr>
             </thead>
             <tbody v-if="items && items.length">
@@ -17,6 +18,27 @@
                     <td>{{ item.category_name }}</td>
                     <td>{{ item.active }}</td>
                     <td>{{ item.created_at }}</td>
+                    <td>
+                        <a href="#" class="btn btn-outline-primary btn-sm" @click.prevent="setItemId(item.id)">
+                            <EditAction />
+                        </a>
+                        <span v-if="true">&nbsp;</span>
+                        <a href="{{ $deleteRoute }}" class="btn btn-outline-danger btn-sm">
+                            <DeleteAction />
+                        </a>
+                    </td>
+
+                </tr>
+                <tr :class="isEdit(item.id) ? '' : 'd-none'">
+                    <th scope="row">{{ item.id }}</th>
+                    <td><input type="text" class="form-control"></td>
+                    <td><input type="checkbox" class="form-check-input">&nbsp Active</td>
+                    <td></td>
+                    <td>
+                        <a href="#" class="btn btn-success btn-sm" @click.prevent="setItemId(null)">Update</a>
+
+                    </td>
+
                 </tr>
             </template>
             </tbody>
@@ -33,23 +55,28 @@
 
 <script>
 import CreateComponent from "@/components/CreateComponent.vue";
+import EditAction from "@/components/actions/EditAction.vue";
+import DeleteAction from "@/components/actions/DeleteAction.vue";
 
 export default {
     name: 'TableComponent',
 
     components: {
         CreateComponent,
+        EditAction,
+        DeleteAction,
     },
 
     data() {
         return {
             items: [],
+            editItemId: null,
         };
     },
 
     computed: {
         active() {
-            return this.items.filter((item) => item.active === 1);
+            return this.items.filter((item) => item.active === 1)
         }
     },
 
@@ -61,14 +88,24 @@ export default {
         getItems() {
             axios.get('api/categories')
                 .then((response) => {
-                    this.items = response.data;
-                    //console.log(this.items);
+                    this.items = response.data
+                    //console.log(this.items)
                 })
                 .catch((error) => {
-                    console.error('Error fetching items:', error);
+                    console.error('Error fetching items:', error)
                 });
+        },
+
+        setItemId(id) {
+            this.editItemId = id
+        },
+
+        isEdit(id) {
+            return this.editItemId === id
         }
-    }
+    },
+
+
 }
 
 </script>
