bc10e0a7f67220e2f8f8411080ed96253ceb6369
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-06 09:17:43 +0100

    Javascript и Vuejs для Laravel 31. SPA приложение. Работа с api resource. Валидация для пустых полей

---
+++ app/Http/Resources/Category/CategoryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Category;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'category_name' => $this->category_name,
+            'category_description' => $this->category_description,
+            'active' => $this->active,
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::parse($this->updated_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
