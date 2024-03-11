963fc7f973212183d6828a6ca9931a539c4ee9fa
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-11 07:53:45 +0100

    change components registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,9 +28,9 @@ app.component('index', Index);
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-// });
+Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+    app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+});
 
 /**
  * Finally, we will attach the application instance to a HTML element with
