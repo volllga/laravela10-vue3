84d3a0b8f415dc0bad58aded4017e0acd0a98e6c
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-16 08:57:54 +0100

    Show.php  after chat gpt

--- resources/js/components/Category/Show.vue
+++ resources/js/components/Category/Show.vue
@@ -1,6 +1,5 @@
 <template>
-
-    <tr :class="this.$parent.isEdit(item.id) ? 'd-none' : ''">
+    <tr :class="$parent.isEdit(item.id) ? 'd-none' : ''">
         <th scope="row"><router-link :to="{ name: 'category.show', params:{id: item.id}}">{{ item.id }}</router-link></th>
         <td>{{ item.category_name }}</td>
         <td>{{ item.category_description }}</td>
@@ -8,64 +7,40 @@
         <td>{{ item.created_at }}</td>
         <td>{{ item.updated_at }}</td>
         <td>
-            <a href="#" class="btn btn-outline-primary btn-sm"
-               @click.prevent="edit(item.id, item.category_name, item.category_description, item.active)">
+            <a href="#" class="btn btn-outline-primary btn-sm" @click.prevent="edit(item)">
                 <EditAction/>
             </a>
-            <span v-if="true">&nbsp;</span>
+            <span>&nbsp;</span>
             <a href="#" class="btn btn-outline-danger btn-sm" @click.prevent="$store.dispatch('destroy', item.id)">
                 <DeleteAction/>
             </a>
         </td>
-
     </tr>
-
 </template>
-
 
 <script>
 import EditAction from "@/components/actions/EditAction.vue";
 import DeleteAction from "@/components/actions/DeleteAction.vue";
 
-
 export default {
     name: 'Show',
-
     components: {
         EditAction,
         DeleteAction,
-
     },
-
-    data() {
-        return {}
-    },
-
-    props: [
-        'item'
-    ],
-
-    computed: {
-    },
-
-    mounted() {
-
-    },
-
+    props: ['item'],
     methods: {
-
-        edit(id, name, description, active) {
-
-            this.$parent.editItemId = id;
-            const editComponent = this.$parent.$refs[`${id}`][0];
+        edit(item) {
+            this.$parent.editItemId = item.id;
+            const editComponent = this.$parent.$refs[`${item.id}`][0];
             if (editComponent) {
                 Object.assign(editComponent, {
-                    categoryName: name,
-                    categoryDescription: description,
-                    active: active
+                    categoryName: item.category_name,
+                    categoryDescription: item.category_description,
+                    active: item.active
                 });
             } else {
-                console.error(`Child component with id ${id} not found.`);
+                console.error(`Child component with id ${item.id} not found.`);
             }
         },
     },
