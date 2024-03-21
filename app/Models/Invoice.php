efe73a2b54ddf02cfc19730c0ef13aa69a665bb9
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-21 09:03:54 +0100

    create model, controller, resourse, request, migration for Invoice

---
+++ app/Models/Invoice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Invoice extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'employees';
+    protected $guarded = false;
+
+    protected $casts = [
+        'active' => 'boolean',
+        'created_at' => 'date:Y-m-d',
+        'updated_at' => 'date:Y-m-d',
+        'deleted_at' => 'date:Y-m-d',
+    ];
+
+}
