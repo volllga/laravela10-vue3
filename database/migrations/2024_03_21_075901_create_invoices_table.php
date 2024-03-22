1e3d48c801643b107eeafb09b49a00f43538c800
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-22 06:35:00 +0100

    create invoice migration

--- database/migrations/2024_03_21_075901_create_invoices_table.php
+++ database/migrations/2024_03_21_075901_create_invoices_table.php
@@ -13,7 +13,21 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->string('number');
+            $table->date('date');
+            $table->date('service_date');
+            $table->date('due_date');
+            $table->integer('status_id')->default(0);
+            $table->integer('company_id')->default(1);
+            $table->integer('product_id')->default(1);
+            $table->integer('quantity')->default(1);
+            $table->integer('customer_id')->nullable();
+            $table->string('vat')->nullable();
+            $table->decimal('amount', 12, 2)->nullable();
+            $table->enum('currency', ['usd', 'eur', 'pln'])->default('usd');
+            $table->enum('secondary_currency', ['usd', 'eur', 'pln'])->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
