5061f218822c4130e0fcffd9180f464e03875d77
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-23 07:13:49 +0100

    add customer resource

--- app/Http/Resources/Customer/CustomerResource.php
+++ app/Http/Resources/Customer/CustomerResource.php
@@ -12,8 +12,20 @@ class CustomerResource extends JsonResource
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'company_name' => $this->company_name,
+            'email' => $this->email,
+            'address' => $this->address,
+            'city' => $this->city,
+            'country' => $this->country,
+            'phone' => $this->phone,
+            'tax_identifier' => $this->tax_identifier,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
     }
 }
