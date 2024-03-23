dd6de4099fe459baf5833dae011b0167e3e4458f
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-23 07:19:16 +0100

    add customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'customers';
+    protected $guarded = [];
+
+    protected $casts = [
+
+        'created_at' => 'date:Y-m-d',
+        'updated_at' => 'date:Y-m-d',
+        'deleted_at' => 'date:Y-m-d',
+    ];
+
+    protected function companyName(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => strtoupper($value),
+            set: fn (string $value) => strtoupper($value),
+        );
+    }
 }
