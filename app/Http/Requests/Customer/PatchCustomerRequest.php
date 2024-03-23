60ffeaf1c958588cb1352ef4152d48f1571d0349
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-23 07:59:59 +0100

    handle customer company_name in patch

--- app/Http/Requests/Customer/PatchCustomerRequest.php
+++ app/Http/Requests/Customer/PatchCustomerRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Customer;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class PatchCustomerRequest extends FormRequest
 {
@@ -24,8 +25,17 @@ class PatchCustomerRequest extends FormRequest
      */
     public function rules(): array
     {
+        $customerId = $this->route('customer') ? $this->route('customer')->id : null;
+
         return [
-            'company_name' => 'sometimes|required|string|max:255|unique:customers,company_name,' . $this->customer,
+            'company_name' => [
+                'sometimes',
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('customers', 'company_name')->ignore($customerId)->whereNull('deleted_at'),
+            ],
+
             'email' => 'sometimes|required|string|email|max:255',
             'address' => 'nullable|string|max:255',
             'city' => 'nullable|string|max:255',
