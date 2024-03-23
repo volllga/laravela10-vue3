86633bea0ed53c7da99f27418311c42da782c1d2
Volha Lukashevich <volllga.goo@gmail.com>
2024-03-23 07:27:38 +0100

    add post, patch customer request

--- app/Http/Requests/Customer/PatchCustomerRequest.php
+++ app/Http/Requests/Customer/PatchCustomerRequest.php
@@ -8,21 +8,30 @@ class PatchCustomerRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
     public function authorize(): bool
     {
-        return false;
+        // Update this to your authorization logic, if necessary
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, mixed>
      */
     public function rules(): array
     {
         return [
-            //
+            'company_name' => 'sometimes|required|string|max:255|unique:customers,company_name,' . $this->customer,
+            'email' => 'sometimes|required|string|email|max:255',
+            'address' => 'nullable|string|max:255',
+            'city' => 'nullable|string|max:255',
+            'country' => 'nullable|string|max:255',
+            'phone' => 'nullable|string|max:255',
+            'tax_identifier' => 'nullable|string|max:255',
         ];
     }
 }
--- app/Http/Requests/Customer/PostCustomerRequest.php
+++ app/Http/Requests/Customer/PostCustomerRequest.php
@@ -8,21 +8,29 @@ class PostCustomerRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, mixed>
      */
     public function rules(): array
     {
         return [
-            //
+            'company_name' => 'required|string|max:255|unique:customers',
+            'email' => 'required|string|email|max:255',
+            'address' => 'nullable|string|max:255',
+            'city' => 'nullable|string|max:255',
+            'country' => 'nullable|string|max:255',
+            'phone' => 'nullable|string|max:255',
+            'tax_identifier' => 'nullable|string|max:255',
         ];
     }
 }
